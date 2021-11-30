a0059d061634605af0e07f43299235880590d40e
Jess Pesale <jess_pesale@aol.com>
2021-11-30 12:15:23 -0500

    fixed routes so can only access items for a specific trip

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
 namespace :api do
     namespace :v1 do
-      resources :itinerary_items
-      resources :packing_items
-      resources :trips
-     end
+      resources :trips do
+        resources :itinerary_items
+        resources :packing_items
+        #can only access itinerary/packing items nested in a trip id
+      end
+    end
   end
 end
 
@@ -13,3 +15,4 @@ end
 
 # fetch('http://localhost::3000')
 # fetch('http://localhost::3000/api/v1/trips')
+# fetch('http://localhost::3000/api/v1/trips/1/packing_items')
