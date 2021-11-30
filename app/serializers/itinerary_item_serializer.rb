7a2d7dd60cbaa08161cd258ca8a396fcaaf3ded6
Jess Pesale <jess_pesale@aol.com>
2021-11-30 13:54:36 -0500

    added serializers attributes for all models

--- app/serializers/itinerary_item_serializer.rb
+++ app/serializers/itinerary_item_serializer.rb
@@ -1,3 +1,3 @@
 class ItineraryItemSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :description
 end
--- app/serializers/packing_item_serializer.rb
+++ app/serializers/packing_item_serializer.rb
@@ -1,3 +1,3 @@
 class PackingItemSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :trip_id, :item
 end
