550419342e67a87884a841af42e8bd456d1a6e3d
Jess Pesale <jess_pesale@aol.com>
2021-11-30 12:14:30 -0500

    set up packing items controller

--- app/controllers/api/v1/packing_items_controller.rb
+++ app/controllers/api/v1/packing_items_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::PackingItemsController < ApplicationController
+
+    before_action :find_trip
+
+    def index 
+        @packing_items = @trip.packing_items
+        render json: @packing_items
+    end
+
+    def create
+        @packing_item = @trip.packing_items.build(packing_item_params)
+        # @packing_item = @trip.packing_items.new(packing_item_params)
+        render json: @packing_item
+    end
+
+    def show 
+        # @packing_item = @trip.packing_items.find_by(id: params[:id])
+        @packing_item = PackingItem.find(params[:id])
+        render json: @packing_item
+    end
+
+    def destroy 
+        @packing_item = @trip.packing_items.find_by(id: params[:id])
+        @packing_item.destroy
+    end
+
+
+    private
+
+    def find_trip
+        @trip = Trip.find(params[:trip_id])
+    end
+
+    def packing_item_params 
+        params.require(:packing_item).permit(:item, :trip_id)
+    end
+
 end
