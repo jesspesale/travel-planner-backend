028041f61202ec8352c6c9d2768518de885afe95
Jess Pesale <jess_pesale@aol.com>
2021-11-30 11:31:45 -0500

    added api/v1 folders and namespaced routes

--- app/controllers/itinerary_items_controller.rb
+++
@@ -1,2 +0,0 @@
-class ItineraryItemsController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :itinerary_items
-  resources :packing_items
-  resources :trips
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+namespace :api do
+    namespace :v1 do
+      resources :itinerary_items
+      resources :packing_items
+      resources :trips
+     end
+  end
 end
+
+
+
+# fetch('http://localhost::3000')
+# fetch('http://localhost::3000/api/v1/trips')
