3bd1f8c3a453dc98918ba628ffa9115b305495d5
Jess Pesale <jess_pesale@aol.com>
2021-11-30 11:40:25 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+trip1 = Trip.create(destination: "Hawaii", start_date: "2022-02-16", end_date: "2022-02-24")
+bathingsuit = PackingItem.create(trip_id: 1, item: "bathing suit" )
+hat = PackingItem.create(trip_id: 1, item: "hat" )
+hiking = ItineraryItem.create(trip_id: 1, description: "Go on a nice hike!")
+trip2 = Trip.create(destination: "Florida", start_date: "2021-12-14", end_date: "2021-12-19")
+sunscreen = PackingItem.create(trip_id: 2, item: "sunscreen")
+
+# Trip.destroy_all
+
+# can run rake db: drop  ->  rake db:migrate  ->  rake db:seed again
